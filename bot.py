25240383cc58d7f974546fe70a2c52ed11548d41
Javi Gomez <rtomyj@gmail.com>
2020-02-05 22:56:33 -0600

    New command to get stats of previous match

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import threading
 from multiprocessing import Process
 
+from discord import Embed, Colour
 from discord.ext import commands
 from dotenv import load_dotenv
 import requests
@@ -52,8 +53,7 @@ def get_ranked_results(username, userId):
 	return f'Solo/Duo Queue Ranked stats for ***{username}***\n**Rank:** {response["tier"]}-{response["rank"]} {response["leaguePoints"]}LP\n**Wins:** {response["wins"]}\n**Losses:** {response["losses"]}\n{hotstreak}'
 
 
-@bot.command(name='rank-solo', help='List stats for solo rank - add username after command.')
-async def rank_solo(context, *usernameTokens):
+def parse_username_tokens(usernameTokens):
 	username = ''
 
 	for token in usernameTokens:
@@ -61,6 +61,13 @@ async def rank_solo(context, *usernameTokens):
 
 	username = username.rstrip()
 	urlFriendlyUsername = username.replace(' ', '%20')
+	return username, urlFriendlyUsername
+
+
+@bot.command(name='rank-solo', help='List stats for solo rank - add username after command.')
+async def rank_solo(context, *usernameTokens):
+	username, urlFriendlyUsername = parse_username_tokens(usernameTokens)
+
 
 	url = f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{ urlFriendlyUsername }'
 	print(f'Using {url} to get summoner ID')
@@ -90,6 +97,82 @@ async def rank_solo(context, *usernameTokens):
 			return
 
 
+def get_match_history(username, urlFriendlyUsername):
+	url = f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{ urlFriendlyUsername }'
+
+	r = requests.get(url, headers=get_headers())
+	json = r.json()
+
+	try:
+		accountId = json['accountId']
+		url = f'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/{ accountId }'
+
+		r = requests.get(url, headers=get_headers())
+		return r.json()['matches']
+
+	except KeyError as exception:
+		if r.status_code == 403:
+			print('Riot API has expired.')
+			return
+		elif r.status_code == 404:
+			print(f'{username} is not a real user.')
+			return
+
+def get_most_recent_game_id(username, urlFriendlyUsername):
+	matches = get_match_history(username, urlFriendlyUsername)
+	return matches[0]['gameId']
+
+def get_match_data_by_game_id(gameId):
+	url = f'https://na1.api.riotgames.com/lol/match/v4/matches/{ gameId }'
+	r = requests.get(url, headers=get_headers())
+
+	return r.json()
+
+
+@bot.command(name='last-match-stats', help='Displays info about your last game.')
+async def get_last_match_stats(context, *usernameTokens):
+	global championData
+	username, urlFriendlyUsername = parse_username_tokens(usernameTokens)
+	summonerId = ''
+
+	url = f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{ urlFriendlyUsername }'
+	print(f'Using {url} to get summoner ID')
+
+	r = requests.get(url, headers = get_headers())
+
+	try:
+		response = r.json()
+		summonerId = response['id']
+	except:
+		print('Error fetching username')
+
+	gameId = get_most_recent_game_id(username, urlFriendlyUsername)
+	riotMatchData = get_match_data_by_game_id(gameId)
+	embedMatchData = dict()	# Data structure to hold info for embed
+
+	for participantData in riotMatchData['participantIdentities']:
+		if participantData['player']['summonerId'] == summonerId:
+			embedMatchData['participantId'] = participantData['participantId']
+			embedMatchData['summonerName'] = participantData['player']['summonerName']
+
+	participantData = riotMatchData['participants'][embedMatchData['participantId'] - 1]
+	participantStats = participantData['stats']
+	embedMatchData['team'] = ('Blue Side', 'Red Side')[participantData['teamId'] == 100 ]
+	embedMatchData['championId'] = str( participantData['championId'] )
+	embedMatchData['kills'] = participantStats['kills']
+	embedMatchData['deaths'] = participantStats['deaths']
+	embedMatchData['assists'] = participantStats['assists']
+
+
+	matchMessage = Embed(color=Colour.from_rgb(224, 17, 95), title=f'Last Match For { embedMatchData["summonerName"] }')
+	matchMessage.set_thumbnail(url=f'http://ddragon.leagueoflegends.com/cdn/10.3.1/img/champion/{ championData[embedMatchData["championId"]]["championName"] }.png')
+	matchMessage.add_field(name='Team', value=embedMatchData['team'], inline=False)
+	matchMessage.add_field(name='Score', value=f'{ embedMatchData["kills"] }/{ embedMatchData["deaths"] }/{ embedMatchData["assists"] }', inline=True)
+	matchMessage.add_field(name='KDA', value=float( int(embedMatchData["kills"]) / int(embedMatchData["deaths"]) ))
+
+	await context.send(embed=matchMessage)
+
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -99,12 +182,26 @@ def test():
 
 
 if __name__ == '__main__':
-
 	port = int(os.getenv('PORT', 8081))
 	host = '0.0.0.0'
 
 	threading.Thread( target=app.run, args=(), kwargs={'host': host, 'port': port } ).start()
 
-	#p = Process(target=client.run, args=(discordApiToken, ))
-	#p.start()
+	global championData
+	championData = dict()
+
+	try:
+		url = 'http://ddragon.leagueoflegends.com/cdn/10.3.1/data/en_US/champion.json'
+		r = requests.get(url)
+
+		riotChampData = r.json()['data']
+
+		for name, data in riotChampData.items():
+			championData[ data['key'] ] = {'championName': name}
+
+	except Exception as e:
+		print(e)
+		print('Err connecting to Riot champion data endpoint')
+
+
 	bot.run(discordApiToken)
